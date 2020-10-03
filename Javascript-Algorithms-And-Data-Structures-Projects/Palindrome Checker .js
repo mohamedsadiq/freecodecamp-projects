1bc92c1f7a9ee5d4995216702dc4d008244d7c54
Mohamed Sadiq <mohamed.sadiq@outlook.sa>
2020-10-03 21:12:33 +0200

    Add files via upload

---
+++ Javascript-Algorithms-And-Data-Structures-Projects/Palindrome Checker .js
@@ -0,0 +1,21 @@
+function palindrome(str) {
+    // Good luck!
+    str = str.toLowerCase().replace(/[_#$,:|-\s.()]/g, "");
+    let start = 0;
+    let end = str.length-1;
+
+    console.log(str);
+
+    while(start <= end){
+      if(str.charAt(start) !== str.charAt(end)){
+        return false;
+      }
+      start++;
+      end--;
+    }
+    return true;
+  }
+
+
+
+  palindrome("eye");
